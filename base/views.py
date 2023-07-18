7bb93a6141154c6e52303e6edfa914f90186e029
crija <crijacarla@gmail.com>
2023-07-18 15:26:13 -0300

    Add dict dictionary

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developer'},
+]
 
 def home(request):
     return render(request, 'home.html')
