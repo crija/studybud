75e91734e33816002bde4136b0134cbb343a8e31
crija <crijacarla@gmail.com>
2023-07-20 19:13:40 -0300

    Add tables

--- base/models.py
+++ base/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    #host = 
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     descriptions = models.TextField(null=True, blank=True)
     #participants =
@@ -13,3 +21,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Menssage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    bory = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.bory[0:50]
